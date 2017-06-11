55f4be2761f95305c829d4efbfd9a066b08df00b
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-10 22:46:35 -0500

    Add and populate contacts dropdown

--- public/controllers/jobs.js
+++ public/controllers/jobs.js
@@ -6,7 +6,7 @@ jstrackApp.controller('jobs', ['$scope', function($scope) {
       company: 'Garmin',
       position: 'Software Engineer I',
       lastContact: 'June 12, 2017',
-      contacts: [{name: 'Jessica Zlab', phoneNumber: '913-555-5555', email: 'jzlab@garmin.com'}, {name: 'Katie Allgood', phoneNumber: '913-444-5555', email: 'kallgood@garmin.com'}],
+      contacts: [{name: 'Jessica Zlab', phoneNumber: '913-555-5555', email: 'jz@garmin.com'}, {name: 'Katie Allgood', phoneNumber: '913-444-5555', email: 'ka@garmin.com'}],
       jobListingUrl: 'http://garminjobs.com'
     }
   ]
