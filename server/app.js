c979a3fed4e174d353b2b94590a39ee0c39e00cd
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-22 17:30:48 -0500

    Bug fix: Serve static files

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,9 @@ mongoose.connect('mongodb://localhost/jstrack');
 app.use(bodyParser.json());
 
 routes(app);
+
+//Access client side files
+app.use(express.static(path.join(__dirname, '../public/')));
 
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
