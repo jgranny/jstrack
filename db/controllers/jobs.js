bc93ddd77540bd4c66af40c07ceef18699193e12
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-23 14:31:52 -0500

    Stub out jobs controller

--- db/controllers/jobs.js
+++ db/controllers/jobs.js
@@ -1,5 +1,19 @@
 const Job = require('../models/jobs');
 
 module.exports = {
+  listJobs(req, res, next) {
 
+  },
+
+  createJob(req, res, next) {
+
+  },
+
+  editJob(req, res, next) {
+
+  },
+
+  deleteJob(req, res, next) {
+
+  }
 };
