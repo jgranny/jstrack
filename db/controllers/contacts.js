b37c1562c5ff31238dc9e46a53f1d2097b56e4d5
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-06-23 14:30:25 -0500

    Stub out contacts controller

--- db/controllers/contacts.js
+++ db/controllers/contacts.js
@@ -2,4 +2,20 @@ const Contact = require('../models/contacts');
 
 module.exports = {
 
+  getContacts(req, res, next) {
+
+  },
+
+  createContact(req, res, next) {
+
+  },
+
+  editContact(req, res, next) {
+
+  },
+
+  deleteContact(req, res, next) {
+
+  }
+
 };
